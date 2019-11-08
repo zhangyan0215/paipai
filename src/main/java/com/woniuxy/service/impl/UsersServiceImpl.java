8c6db9dccf6b20bb9045b97c6863987387832655
zhangyan <528832694@qq.com>
2019-11-08 21:50:12 +0800

    测试

--- src/main/java/com/woniuxy/service/impl/UsersServiceImpl.java
+++ src/main/java/com/woniuxy/service/impl/UsersServiceImpl.java
@@ -20,7 +20,7 @@ public class UsersServiceImpl implements IUsersService {
 	private UsersMapper mapper;
 	
 	@Autowired
-	private UsersRolesMapper urm;
+	private UsersRolesMapper urm; 
 	    
 
 	@Transactional
